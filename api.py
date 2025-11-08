89b8c08f848e6a3410a0226c51a2c8c47cfc6941
Kyle D. Ross <6475128+kyledross@users.noreply.github.com>
2025-11-08 04:28:09 -0500

    Added event scores to response to allow client-side filtering

--- api.py
+++ api.py
@@ -18,7 +18,11 @@ class WeatherAlertResponse(BaseModel):
     headline: Optional[str] = None
     event: Optional[str] = None
     severity: Optional[str] = None
+    severity_score: Optional[int] = None
     urgency: Optional[str] = None
+    urgency_score: Optional[int] = None
+    certainty: Optional[str] = None
+    certainty_score: Optional[int] = None
     expires: Optional[str] = None
     description: Optional[str] = None
     instruction: Optional[str] = None
@@ -41,11 +45,35 @@ def get_weather_alert(state: str, city: str):
         }
 
         if alert:
+            severity_score_map = {
+                "UNKNOWN": 0,
+                "MINOR": 1,
+                "MODERATE": 2,
+                "SEVERE": 3,
+                "EXTREME": 4
+            }
+            urgency_score_map = {
+                "UNKNOWN": 0,
+                "FUTURE": 1,
+                "EXPECTED": 2,
+                "IMMEDIATE": 3
+            }
+            certainty_score_map = {
+                "UNKNOWN": 0,
+                "UNLIKELY": 1,
+                "POSSIBLE": 2,
+                "LIKELY": 3,
+                "OBSERVED": 4
+            }
             response.update({
                 "headline": alert.headline,
                 "event": alert.event,
                 "severity": alert.severity.name,
+                "severity_score": severity_score_map.get(alert.severity.name, 0),
                 "urgency": alert.urgency.name,
+                "urgency_score": urgency_score_map.get(alert.urgency.name, 0),
+                "certainty": alert.certainty.name,
+                "certainty_score": certainty_score_map.get(alert.certainty.name, 0),
                 "expires": alert.expires.astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC'),
                 "description": alert.description,
                 "instruction": alert.instruction,
